c338284fb75da9bbccb9537e37f9abf72fc0bf8e
upsilon <varankinigor@gmail.com>
2018-01-17 17:29:23 +0300

    ctrl alt l

--- src/test/java/hw2/Exercise1.java
+++ src/test/java/hw2/Exercise1.java
@@ -13,18 +13,19 @@ public class Exercise1 extends TestBase {
 
     @DataProvider(parallel = true)
     public Object[][] dp() {
-        return new Object[][] {
+        return new Object[][]{
                 {"To include good practices\nand ideas from successful\nEPAM projec"},
                 {"To be flexible and\ncustomizable"},
                 {"To be multiplatform"},
                 {"Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…"}
         };
     }
+
     @Test(dataProvider = "dp")
     public void dpTest(String expectedText) {
         List<WebElement> listElements = driver.findElements(By.className("benefit-txt"));
         List<String> listTexts = new ArrayList<String>();
-        for (WebElement element: listElements) {
+        for (WebElement element : listElements) {
             listTexts.add(element.getText());
         }
         Assert.assertTrue(listTexts.contains(expectedText));
